1b9d345de58bfaa3521a5c05fccc66f3e382b750
EKO OKDASATYAGAMA <eko.okda@gmail.com>
2022-06-13 09:58:46 +0700

    Update query_product.php

--- bshop/admin/query_product.php
+++ bshop/admin/query_product.php
@@ -6,12 +6,37 @@ $url = isset($_GET['f']) ? $_GET['f'] : "";
 
 if ($url == 'update_product') {
     update_product($db);
+} else if ($url == 'add_product') {
+    add_product($db);
 } else if ($url == 'get_all_product') {
     get_all_product($db);
 } else if ($url == 'get_product_by_id') {
     get_product_by_id($db, $_GET['id']);
 } else if ($url == 'delete_product') {
     delete_product($db);
+} else {
+    header("location: login.php");
+}
+
+function add_product($db)
+{
+    $product_name = validasi_input($_POST['fProductName']);
+    $sku = validasi_input($_POST['fsku']);
+    $stok = validasi_input($_POST['fStok']);
+    $harga_satuan = validasi_input($_POST['fHargaSatuan']);
+    $image_url = $_POST['fImageProduct'];
+
+    $query = "INSERT INTO produk (product_name, sku, stok, harga_satuan, image_url) VALUES ('$product_name', '$sku', '$stok', '$harga_satuan', '$image_url')";
+
+    if ($db->query($query) === TRUE) {
+        $data = array(
+            'status' => 1,
+            'message' => 'Register berhasil. Silahkan login!'
+        );
+        echo json_encode($data);
+    } else {
+        echo json_encode(array('status' => 0, 'message' => "Error: " . $query . "<br>" . $db->error));
+    }
 }
 
 function update_product($db)
