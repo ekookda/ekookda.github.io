40a441716392fa90eae6c3d4bc9f8df0eefb0bdd
EKO OKDASATYAGAMA <eko.okda@gmail.com>
2022-06-14 13:38:47 +0700

    Delete query get_product

--- bshop/q_product.php
+++ bshop/q_product.php
@@ -7,8 +7,8 @@ $url = isset($_GET['f']) ? $_GET['f'] : "";
 
 if ($url == 'get_all_product') {
     get_all_product($db);
-} else if ($url == 'get_product') {
-    get_product($db);
+// } else if ($url == 'get_product') {
+    // get_product($db);
 } else {
     header("location: index.php");
 }
@@ -18,13 +18,13 @@ function get_all_product($db)
     $sql = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk ORDER BY produk_id DESC";
     $result = $db->query($sql);
     $result_array = $result->fetch_all(MYSQLI_ASSOC);
-    echo json_encode(['data' => $result_array]);
-}
-
-function get_product($db)
-{
-    $sql = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk ORDER BY produk_id DESC";
-    $result = $db->query($sql);
-    $result_array = $result->fetch_all(MYSQLI_ASSOC);
     echo json_encode($result_array);
 }
+
+// function get_product($db)
+// {
+//     $sql = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk ORDER BY produk_id DESC";
+//     $result = $db->query($sql);
+//     $result_array = $result->fetch_all(MYSQLI_ASSOC);
+//     echo json_encode($result_array);
+// }
