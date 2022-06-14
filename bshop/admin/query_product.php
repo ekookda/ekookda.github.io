5c96333bd7172cbf6de46abb963a0ca19cc1f3cc
EKO OKDASATYAGAMA <eko.okda@gmail.com>
2022-06-14 12:05:26 +0700

    Comment script file upload

--- bshop/admin/query_product.php
+++ bshop/admin/query_product.php
@@ -20,40 +20,42 @@ if ($url == 'update_product') {
 
 function add_product($db)
 {
-    $product_name = validasi_input($_POST['nama']);
-    $sku = validasi_input($_POST['sku']);
-    $stok = validasi_input($_POST['stok']);
-    $harga_satuan = validasi_input($_POST['harga_satuan']);
-    $image_name = $_FILES['img_url']['name'];
+    $product_name = validasi_input($_POST['product_name']);
+    $product_sku = validasi_input($_POST['product_sku']);
+    $product_stok = (int) validasi_input($_POST['product_stok']);
+    $product_price = validasi_input($_POST['product_price']);
+    // $product_image = (file_exists($_FILES['product_image']['name']) || is_uploaded_file($_FILES['product_image']['name'])) ? $_FILES['product_image']['name'] : "";
 
-    if ($image_name != "") {
-        // Ekstensi yang diperbolehkan untuk diupload boleh diubah sesuai keinginan
-        $ekstensi_izin = array('png', 'jpg', 'jpeg');
-        // Memisahkan nama file dengan Ekstensinya
-        $pisahkan_ekstensi = explode('.', $image_name);
-        $ekstensi = strtolower(end($pisahkan_ekstensi));
-        // Nama file yang berada di dalam direktori temporer server
-        $file_tmp = $_FILES['img_url']['tmp_name'];
-        // Membuat angka/huruf acak berdasarkan waktu diupload
-        $image_date = md5(date('Y-m-d h:i:s'));
-        // Menyatukan angka/huruf acak dengan nama file aslinya
-        $image_name_new = $image_date . '-' . $image_name;
-        // Mengecek apakah Ekstensi file sesuai dengan Ekstensi file yg diuplaod
-        if (in_array($ekstensi, $ekstensi_izin) === true) {
-            // Memindahkan File kedalam Folder "FOTO"
-            move_uploaded_file($file_tmp, 'images/product/main/' . $image_name_new);
-            $query = "INSERT INTO produk (nama, sku, stok, harga_satuan, img_url) VALUES ('$product_name', '$sku', '$stok', '$harga_satuan', '$image_name_new')";
-        }
-    } else {
-        $query = "INSERT INTO produk (nama, sku, stok, harga_satuan) VALUES ('$product_name', '$sku', '$stok', '$harga_satuan')";
-    }
+    // if ($image_name != "") {
+    //     // Ekstensi yang diperbolehkan untuk diupload boleh diubah sesuai keinginan
+    //     $ekstensi_izin = array('png', 'jpg', 'jpeg');
+    //     // Memisahkan nama file dengan Ekstensinya
+    //     $pisahkan_ekstensi = explode('.', $image_name);
+    //     $ekstensi = strtolower(end($pisahkan_ekstensi));
+    //     // Nama file yang berada di dalam direktori temporer server
+    //     $file_tmp = $_FILES['img_url']['tmp_name'];
+    //     // Membuat angka/huruf acak berdasarkan waktu diupload
+    //     $image_date = md5(date('Y-m-d h:i:s'));
+    //     // Menyatukan angka/huruf acak dengan nama file aslinya
+    //     $image_name_new = $image_date . '-' . $image_name;
+    //     // Mengecek apakah Ekstensi file sesuai dengan Ekstensi file yg diuplaod
+    //     if (in_array($ekstensi, $ekstensi_izin) === true) {
+    //         // Memindahkan File kedalam Folder "FOTO"
+    //         move_uploaded_file($file_tmp, 'images/product/main/' . $image_name_new);
+    //         $query = "INSERT INTO produk (nama, sku, stok, harga_satuan, img_url) VALUES ('$product_name', '$sku', '$stok', '$harga_satuan', '$image_name_new')";
+    //     }
+    // } else {
+    // $query = "INSERT INTO produk (nama, sku, stok, harga_satuan, img_url) VALUES ('$product_name', '$product_sku', '$product_stok', '$product_price','$product_image')";
+    $query = "INSERT INTO produk (nama, sku, stok, harga_satuan) VALUES ('$product_name', '$product_sku', '$product_stok', '$product_price')";
+    // }
 
     if ($db->query($query) === TRUE) {
-        $data = array(
-            'status' => 1,
-            'message' => 'Insert produk berhasil!'
+        echo json_encode(
+            array(
+                'status' => 1,
+                'message' => 'Product has been added successfully!'
+            )
         );
-        echo json_encode($data);
     } else {
         echo json_encode(
             array(
@@ -66,50 +68,81 @@ function add_product($db)
 
 function update_product($db)
 {
-    $produk_id = validasi_input($_POST['produk_id']);
-    $product_name = validasi_input($_POST['nama']);
-    $sku = validasi_input($_POST['sku']);
-    $stok = validasi_input($_POST['stok']);
-    $harga_satuan = validasi_input($_POST['harga_satuan']);
-    $image_name = $_FILES['img_url']['name'];
+    if (isset($_POST['product_id'])) {
+        $product_id = validasi_input($_POST['product_id']);
+        $product_name = validasi_input($_POST['update_product_name']);
+        $product_sku = validasi_input($_POST['update_product_sku']);
+        $product_stok = validasi_input($_POST['update_product_stok']);
+        $product_price = validasi_input($_POST['update_product_price']);
+        $product_image = (isset($_FILES['update_product_image']['name']) || $_FILES['update_product_image']['name'] != "") ? $_FILES['update_product_image']['name'] : "";
 
-    if (!is_uploaded_file($image_name) || $image_name == "") { // Jika gambar tidak diupload
-        $sql = "UPDATE produk SET nama='$product_name', sku='$sku', stok='$stok', harga_satuan='$harga_satuan' WHERE produk_id='$produk_id'";
-    } else {
-        $sql = "UPDATE produk SET nama='$product_name', sku='$sku', stok='$stok', harga_satuan='$harga_satuan', img_url='$image_name' WHERE produk_id='$produk_id'";
-    }
+        if (!is_uploaded_file($product_image) || $product_image == "") { // Jika gambar tidak diupload
+            $query = "UPDATE produk SET nama='$product_name', sku='$product_sku', stok='$product_stok', harga_satuan='$product_price' WHERE produk_id='$product_id'";
+        } else {
+            $query = "UPDATE produk SET nama='$product_name', sku='$product_sku', stok='$product_stok', harga_satuan='$product_price', img_url='$product_image' WHERE produk_id='$product_id'";
+        }
 
-    if ($db->query($sql) === TRUE) {
-        echo "Record has been updated successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $db->error;
+        if ($db->query($query) === TRUE) {
+            echo json_encode(
+                array(
+                    'status' => 1,
+                    'message' => 'Record has been updated successfully!'
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    'status' => 0,
+                    'message' => "Error: " . $query . "<br>" . $db->error
+                )
+            );
+        }
     }
+    return false;
 }
 
 function delete_product($db)
 {
-    $produk_id = $_POST['produk_id'];
-    $sql = "DELETE FROM produk WHERE produk_id='$produk_id'";
-    if ($db->query($sql) === TRUE) {
-        echo "Record deleted successfully";
-        header('Location: products.php');
+    if (isset($_POST['product_id'])) {
+        $product_id = $_POST['product_id'];
+        $query = "DELETE FROM produk WHERE produk_id='$product_id'";
+        if ($db->query($query) === TRUE) {
+            echo json_encode(
+                array(
+                    'status' => 1,
+                    'message' => 'Delete produk berhasil!'
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    'status' => 0,
+                    'message' => "Error: " . $query . "<br>" . $db->error
+                )
+            );
+        }
     } else {
-        echo "Error deleting record: " . $db->error;
+        echo json_encode(
+            array(
+                'status' => 0,
+                'message' => "Product ID not found!"
+            )
+        );
     }
 }
 
 function get_all_product($db)
 {
-    $sql = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk ORDER BY produk_id DESC";
-    $result = $db->query($sql);
+    $query = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk ORDER BY produk_id DESC";
+    $result = $db->query($query);
     $result_array = $result->fetch_all(MYSQLI_ASSOC);
     echo json_encode(['data' => $result_array]);
 }
 
 function get_product_by_id($db, $produk_id)
 {
-    $sql = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk WHERE produk_id = '$produk_id'";
-    $result = $db->query($sql);
+    $query = "SELECT produk_id, nama, sku, stok, harga_satuan, img_url FROM produk WHERE produk_id = '$produk_id'";
+    $result = $db->query($query);
     $result_array = $result->fetch_all(MYSQLI_ASSOC);
     echo json_encode($result_array[0]);
 }
