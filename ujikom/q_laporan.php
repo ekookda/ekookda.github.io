fe2ffbd3cc0e53102a3eda9df81ecd8dd6badf7c
EKO OKDASATYAGAMA <eko.okda@gmail.com>
2022-06-15 23:46:06 +0700

    Update q_laporan.php

--- ujikom/q_laporan.php
+++ ujikom/q_laporan.php
@@ -7,6 +7,16 @@ if ($get == 'get_laporan') {
     get_laporan($conn);
 } 
 
-function get_laporan($conn){
-    
+function get_laporan($conn)
+{
+    $query = "SELECT kode_barang, nama_barang, harga_jual, SUM(jumlah_barang) AS JumlahPenjualan, tbl_barang.satuan, SUM(sub_total) AS TotalPenjualan 
+                FROM tbl_penjualan 
+                LEFT JOIN tbl_detail_penjualan 
+                USING (no_penjualan) 
+                LEFT JOIN tbl_barang 
+                USING (nama_barang) 
+                GROUP BY kode_barang";
+    $result = $conn->query($query);
+    $result_array = $result->fetch_all(MYSQLI_ASSOC);
+    echo json_encode(array('data' => $result_array));
 }
